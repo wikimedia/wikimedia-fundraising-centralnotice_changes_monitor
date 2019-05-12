8b702ffc389a5846601c5c25a0ed408ceaa96d3e
Andrew Green <andrew.green.df@gmail.com>
2019-05-12 11:42:14 -0500

    Rename module page_monitor -> page_manager

--- centralnotice_changes_monitor/controller.py
+++ centralnotice_changes_monitor/controller.py
@@ -1,7 +1,7 @@
 import logging
 
 from centralnotice_changes_monitor import ( wiki_api, db, campaign_manager, banner_manager,
-    page_monitor )
+    page_manager )
 
 from centralnotice_changes_monitor.alert_pattern import AlertPattern
 
@@ -28,7 +28,7 @@ def stream_changes( wiki_api_settings, db_settings, alert_pattern_settings ):
     campaigns = campaign_manager.campaigns()
     banners = banner_manager.from_campaigns( campaigns )
     transcluded_pages = banner_manager.transcluded_pages( banners )
-    pages_to_monitor = page_monitor.pages_to_monitor( banners, transcluded_pages )
+    pages_to_monitor = page_manager.pages_to_monitor( banners, transcluded_pages )
 
     # Unlike transcluded pages, we don't get banners' latest revision when the objects
     # are created, so we fetch that data separately.
@@ -36,18 +36,18 @@ def stream_changes( wiki_api_settings, db_settings, alert_pattern_settings ):
 
     # Find and set pages' revision checked and status properties with regard to state stored
     # in the db, and get a list of pages no longer in monitoring.
-    removed_pages = page_monitor.set_properties_from_db_and_get_removed( pages_to_monitor )
+    removed_pages = page_manager.set_properties_from_db_and_get_removed( pages_to_monitor )
 
     # Check for alerts looking at changes in pages to monitor and removed pages
     for page in pages_to_monitor + removed_pages:
-        page_monitor.set_changes( page )
-        alerts = page_monitor.get_alerts( page, alert_patterns )
+        page_manager.set_changes( page )
+        alerts = page_manager.get_alerts( page, alert_patterns )
 
         # Output alerts
         for alert in alerts:
             print( alert.output() )
 
-        page_monitor.update_page_after_alerts( page )
+        page_manager.update_page_after_alerts( page )
 
     db.close()
 
