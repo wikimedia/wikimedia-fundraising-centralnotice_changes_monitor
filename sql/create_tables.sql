57df2ef4bccc79413a5023768f981a82027c7ee6
Andrew Green <andrew.green.df@gmail.com>
2019-05-25 11:02:12 -0500

    Limit db title key length for older versions of mysql/mariadb

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,7 +1,7 @@
 -- Create tables used by this library/scripts. See README.md for details.
 CREATE TABLE pages_to_monitor (
   id               INT           UNSIGNED AUTO_INCREMENT,
-  title            VARCHAR(256)  NOT NULL,
+  title            VARCHAR(255)  NOT NULL,
   checked_revision INT           UNSIGNED NOT NULL,
 
   PRIMARY KEY (id),
